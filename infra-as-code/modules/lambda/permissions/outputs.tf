b7c6722e11d607b278465416757851d73b699a7d
gPass0s <guilhermepassos@outlook.com>
2024-01-05 17:15:06 -0300

    Remove permision module from lambda terraform module

--- infra-as-code/modules/lambda/permissions/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "role-arn" {
-  value       = aws_iam_role.iam_for_lambda.arn
-  description = "Lambda IAM Role ARN"
-}
-
-output "role-name" {
-  value       = aws_iam_role.iam_for_lambda.name
-  description = "Lambda IAM Role ARN"
-}
