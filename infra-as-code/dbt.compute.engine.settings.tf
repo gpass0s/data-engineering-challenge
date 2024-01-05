5554d1607c5bcba342d7f61ab6d19caf20a5ff42
gPass0s <guilhermepassos@outlook.com>
2024-01-05 00:24:57 -0300

    Introduce a terraform file that defines the a compute layer to run dbt containers

---
+++ infra-as-code/dbt.compute.engine.settings.tf
@@ -0,0 +1,27 @@
+
+module "ecs-cluster-for-dbt" {
+  source          = "./modules/ecs-cluster"
+  PROJECT_NAME    = local.PROJECT_NAME
+  ENV             = local.ENV
+  RESOURCE_SUFFIX = "cluster-for-dbt"
+  AWS_TAGS        = var.AWS_TAGS
+}
+
+module "ecs-task-definition" {
+  source              = "./modules/ecs-task-definition"
+  ENV                 = local.ENV
+  PROJECT_NAME        = local.PROJECT_NAME
+  AWS_TAGS            = local.AWS_TAGS
+  RESOURCE_SUFFIX     = "task-definition-for-dbt"
+  ECR_REPOSITORY_URL  = module.ecr-repository.repository_url
+  ECR_IMAGE_NAME      = "dbt-jaffle-shop-latest"
+  DBT_ECS_CLUSTER_ARN = module.ecs-cluster-for-dbt.arn
+}
+
+module "ecr-repository" {
+  source          = "./modules/ecr"
+  ENV             = local.ENV
+  PROJECT_NAME    = local.PROJECT_NAME
+  AWS_TAGS        = local.AWS_TAGS
+  RESOURCE_SUFFIX = "images-repository"
+}
